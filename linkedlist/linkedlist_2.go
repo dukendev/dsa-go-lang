d8858daa752b7168181136269bce01b934fbf9e2
dukendev <ysanjeet535@gmail.com>
2024-02-18 09:30:07 +0530

    minor
    minor

---
+++ linkedlist/linkedlist_2.go
@@ -0,0 +1,20 @@
+package linkedlist
+
+// todo : add list At start
+func (l *LinkedList[T]) AppendAtStart(list LinkedList[T]) {
+
+}
+
+//todo : add list At end
+
+//todo : reverse list
+
+//todo : sort list
+
+//todo : merge two sorted list
+
+//todo : detech cycle
+
+//todo : find cycle
+
+//todo : reverse sublist
